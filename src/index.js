533dcf55bc2b9192aeb40aa949c859fecbc00f14
Gaudou Noémie <gaudou.noemie@gmail.com>
2025-01-28 11:18:05 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+import About from './Pages/About';
+import Cv from './Pages/Cv';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-// Lazy load the pages
-const Home = lazy(() => import('./Pages/Home'));
-const About = lazy(() => import('./Pages/About'));
-const Cv = lazy(() => import('./Pages/Cv'));
-const Portfolio = lazy(() => import('./Pages/Portfolio'));
-const Error = lazy(() => import('./Pages/Error'));
-
+import Portfolio from './Pages/Portfolio';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Router>
-      <Header />
-      <main className="container">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/cv" element={<Cv />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Suspense>
-      </main>
-      <Footer />
+      <Header/>
+      <main className='container'>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/portfolio' element={<Portfolio/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/cv' element={<Cv/>}/>
+      <Route path='*' element={<Error/>}/> 
+    </Routes>
+    </main>
+    <Footer/>
     </Router>
-  </React.StrictMode>
+   </React.StrictMode>,
+  
 );
